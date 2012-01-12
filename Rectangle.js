08be5132adc3444a3d09d9f83cd7952cda8d2eff
Daniel Freedman <compismyrx@gmail.com>
2012-01-11 16:25:13 -0800

    basic rectangle canvas control

---
+++ Rectangle.js
@@ -0,0 +1,13 @@
+enyo.kind({
+	name: "enyo.canvas.Rectangle",
+	kind: enyo.canvas.Control,
+	published: {
+		//* fill, stroke, or clear
+		style: "fill"
+	},
+	renderSelf: function(ctx) {
+		if (this.style != "clear") {
+			ctx[this.style + "Style"] = this.color;
+		}
+		ctx[this.style + "Rect"](this.bounds.l, this.bounds.t, this.bounds.w, this.bounds.h);
+	}
