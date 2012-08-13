5a103f74c2a00303002636e2f99106abb6bf758a
Jim Tang <jdtang@cww.org>
2012-08-13 10:57:03 -0700

    Fixed syntax of @protected marker. Enyo-DCO-1.0-Signed-off-by: Jim Tang <jim.tang@palm.com>

--- Circle.js
+++ Circle.js
@@ -5,7 +5,7 @@
 enyo.kind({
 	name: "enyo.canvas.Circle",
 	kind: enyo.canvas.Shape,
-	//@ protected
+	//* @protected
 	renderSelf: function(ctx) {
 		ctx.beginPath();
 		ctx.arc(this.bounds.l, this.bounds.t, this.bounds.w, 0, Math.PI*2);
